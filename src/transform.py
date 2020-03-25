2f4025a760ff2a0dd7985dd1da18463c59d865ed
hebowei2000 <2312217486@qq.com>
2020-03-25 11:40:01 +0800

    transform: gray to bimage

---
+++ src/transform.py
@@ -0,0 +1,12 @@
+import cv2
+import os
+import glob
+
+PATH = '../data/result'
+paths = glob.glob (os.path.join(PATH,'*.png'))
+paths.sort()
+
+for path in paths:
+    img = cv2.imread(path)
+    ret,thresh = cv2.threshold(img,7,225,cv2.THRESH_BINARY)
+    cv2.imwrite(path,thresh)
