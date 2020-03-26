a0dbfb9ed43ba6aa6956c96b7e16248104a81dd1
hebowei2000 <2312217486@qq.com>
2020-03-26 21:33:07 +0800

    23345

--- src/net/baseline.py
+++ src/net/baseline.py
@@ -25,14 +25,14 @@ class baseline(nn.Module):
 
         x1, x2, x3, x4 = self.resnet(x)
 
-        #x_s1 = self.reduce_s1(x1)
-       # x_s2 = self.reduce_s2(x2)
-       # x_s3 = self.reduce_s3(x3)
+        x_s1 = self.reduce_s1(x1)
+        x_s2 = self.reduce_s2(x2)
+        x_s3 = self.reduce_s3(x3)
         x_s4 = self.reduce_s4(x4)
         
-        x_s1 = self.ppm(x_s1)
-        x_s2 = self.ppm(x_s2)
-        x_s3 = self.ppm(x_s3)
+       # x_s1 = self.ppm(x_s1)
+       # x_s2 = self.ppm(x_s2)
+       # x_s3 = self.ppm(x_s3)
         x_s4 = self.ppm(x_s4)
 
         pred_s = self.output_s(x_s1, x_s2, x_s3, x_s4)
